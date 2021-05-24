53e694400a2caa349f2da76019d2591a2b965f7e
SmeeBear <soheron@gmail.com>
2021-05-24 18:38:55 -0500

    Added FA X and reminder classification

---
+++ src/components/Task.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="[task.reminder ? 'reminder' : '', 'task']"> 
+        <h3>
+            {{task.text}}
+            <i class="fas fa-times"></i>
+        </h3>
+        <p>{{task.day}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    }
+}
+</script>
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,39 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-        <h3>{{ task.text }}</h3>
+        <Task :task="task" />
     </div>
 </template>
 
 <script>
+import Task from './Task'
+
 export default {
     name: "Tasks",
     props: {
         tasks: Array
+    },
+    components: {
+        Task
     }
 }
 </script>
+
+<style>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
